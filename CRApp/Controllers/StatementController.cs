e5eb8e0afda18bb81a89df98ada7b2bc6a7e831d
raj <royal_raj@outlook.com>
2018-01-24 13:57:04 +0545

    CrystalReport1 added

--- CRApp/Controllers/StatementController.cs
+++ CRApp/Controllers/StatementController.cs
@@ -7,8 +7,7 @@ using System.Web.Mvc;
 using CrystalDecisions.CrystalReports.Engine;
 using CRApp.Models;
 using CRApp.Reports;
-//using iTextSharp;
-//using iTextSharp.text.pdf;
+
 
 
 namespace CRApp.Controllers
@@ -24,18 +23,26 @@ namespace CRApp.Controllers
             return View();
         }
 
-        public void Export()
+        public ActionResult Export()
         {
             ReportDocument rd = new ReportDocument();
-            rd.Load(Path.Combine(Server.MapPath("~/Reports/CRStatement.rpt")));
+            rd.Load(Path.Combine(Server.MapPath("~/Reports/CrystalReport1.rpt")));
             rd.SetDataSource(entities.statements.Select(s => new
             {
-                Id = s.Id,
-                TransactionDate = s.TransactionDate,
-                CustomerName = s.CustomerName,
-                CardNumber = s.CardNumber,
-                imagePath = s.imagePath
-               
+                                Id = s.Id,
+                                TransactionDate = s.TransactionDate,
+                                CustomerName = s.CustomerName,
+                                CardNumber = s.CardNumber,
+                                imagePath = s.imagePath,
+
+
+                
+                PostedDate = s.PostedDate,
+                DescriptionOfTransactions = s.DescriptionOfTransactions,
+                TransactionsAmount = s.TransactionsAmount,
+                BilledAmount = s.BilledAmount,
+                DRCR = s.DRCR,
+
             }).ToList());
             Response.Buffer = false;
             Response.ClearContent();
@@ -43,21 +50,21 @@ namespace CRApp.Controllers
             Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
            // stream.Seek(0, SeekOrigin.Begin);
 
-
+            Stream t_Output;
             using (Stream t_Input = stream)
             //using (Stream t_Output = new FileStream(Server.MapPath(".") + "\\test_encrypted.pdf", FileMode.Create, FileAccess.Write, FileShare.None))
-            using (Stream t_Output = new FileStream("C:\\img\\" + "test_encrypted.pdf", FileMode.Create, FileAccess.Write, FileShare.None))
+            using (t_Output = new FileStream("C:\\img\\" + "test_encrypted.pdf", FileMode.Create, FileAccess.Write, FileShare.None))
             {
                 iTextSharp.text.pdf.PdfReader t_Reader = new iTextSharp.text.pdf.PdfReader(t_Input);
-                iTextSharp.text.pdf.PdfEncryptor.Encrypt(t_Reader, t_Output, true, "prsujoy", "sujoy", iTextSharp.text.pdf.PdfWriter.ALLOW_PRINTING);
+                iTextSharp.text.pdf.PdfEncryptor.Encrypt(t_Reader, t_Output, true, "123", "test", iTextSharp.text.pdf.PdfWriter.ALLOW_PRINTING);
             }
 
-            //return File(stream, "application/pdf", "ListStatement.pdf");
-            
-            
-            
-            
-            
+            //return File("C:\\img\\" + "test_encrypted.pdf", "application/pdf", "ListStatement.pdf");
+           
+            return View();
+
+
+
             //PdfDocument document = PdfReader.Open(stream);
 
 
@@ -86,7 +93,7 @@ namespace CRApp.Controllers
 
 
 
-          //return File(stream, "application/pdf", "ListStatement.pdf");
+            //return File(stream, "application/pdf", "ListStatement.pdf");
 
             //FileStream output;
             //using (var input = new FileStream("c:/ListStatement.pdf", FileMode.Open, FileAccess.Read, FileShare.Read))
@@ -98,7 +105,9 @@ namespace CRApp.Controllers
             //}
 
 
-            
+
         }
+
+        
 	}
 }
--- CRApp/Models/ModelStatement.Designer.cs
+++ CRApp/Models/ModelStatement.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\elitenet\documents\visual studio 2013\Projects\CRApp\CRApp\Models\ModelStatement.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Elitenet\Documents\Visual Studio 2013\Projects\CRApp\CRApp\Models\ModelStatement.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
